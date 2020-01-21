5f87ebd4a7643f06992bfdf7d5a60f16f525e482
Albert Xu <albert.t.xu@gmail.com>
2020-01-21 18:33:40 -0500

    add multiprocessing

--- emscalebar.py
+++ emscalebar.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import logging
+from multiprocessing import Pool
 import os
 import tempfile
 import traceback
@@ -12,6 +13,25 @@ from skimage import exposure
 from skimage.util import img_as_ubyte
 import mrcfile
 import numpy as np
+
+parser = argparse.ArgumentParser(
+    description="add scale bar to electron microscopy images"
+)
+parser.add_argument("mrcfiles", help="input mrc files", nargs="+")
+parser.add_argument(
+    "--outputdir", help="output directory for created jpg images", default=os.getcwd(),
+)
+parser.add_argument(
+    "--maxsize", help="maximum image dimension for jpg images", default=1250, type=int,
+)
+parser.add_argument("--numprocs", help="number of parallel processes to run", type=int)
+args = parser.parse_args()
+if not os.path.exists(args.outputdir):
+    print(f"{args.outputdir} was not found. Exiting.")
+    exit()
+if not os.path.isdir(args.outputdir):
+    print(f"{args.outputdir} is not a directory. Exiting.")
+    exit()
 
 
 def read_mrc(path_to_mrc):
@@ -61,50 +81,31 @@ def make_jpg_with_scalebar(img, pixel_size, outputdir, outputname):
     )
 
 
+def process_mrc(path_to_mrc):
+    mrc_data = read_mrc(path_to_mrc)
+    if mrc_data is None:
+        return
+    img = mrc_data[0]
+    pixel_size = mrc_data[1]
+    if max(*img.shape[-2:]) > args.maxsize:
+        shrink_factor = max(*img.shape[-2:]) / args.maxsize
+        pixel_size *= shrink_factor
+    else:
+        shrink_factor = 1
+
+    basename = os.path.split(path_to_mrc)[-1]
+    if len(img.shape) == 2:
+        img = reduce_img(img, shrink_factor)
+        outputname = f"{basename}.jpg"
+        make_jpg_with_scalebar(img, pixel_size, args.outputdir, outputname)
+    elif len(img.shape) == 3:
+        for i in range(img.shape[0]):
+            outputname = f"{basename}_section{i}.jpg"
+            section = img[i]
+            section = reduce_img(section, shrink_factor)
+            make_jpg_with_scalebar(section, pixel_size, outputdir, outputname)
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="add scale bar to electron microscopy images"
-    )
-    parser.add_argument("mrcfiles", help="input mrc files", nargs="+")
-    parser.add_argument(
-        "--outputdir",
-        help="output directory for created jpg images",
-        default=os.getcwd(),
-    )
-    parser.add_argument(
-        "--maxsize",
-        help="maximum image dimension for jpg images",
-        default=1250,
-        type=int,
-    )
-    args = parser.parse_args()
-    if not os.path.exists(args.outputdir):
-        print(f"{args.outputdir} was not found. Exiting.")
-        exit()
-    if not os.path.isdir(args.outputdir):
-        print(f"{args.outputdir} is not a directory. Exiting.")
-        exit()
-
-    for mrc in args.mrcfiles:
-        mrc_data = read_mrc(mrc)
-        if mrc_data is None:
-            continue
-        img = mrc_data[0]
-        pixel_size = mrc_data[1]
-        if max(*img.shape[-2:]) > args.maxsize:
-            shrink_factor = max(*img.shape[-2:]) / args.maxsize
-            pixel_size *= shrink_factor
-        else:
-            shrink_factor = 1
-
-        basename = os.path.split(mrc)[-1]
-        if len(img.shape) == 2:
-            img = reduce_img(img, shrink_factor)
-            outputname = f"{basename}.jpg"
-            make_jpg_with_scalebar(img, pixel_size, args.outputdir, outputname)
-        elif len(img.shape) == 3:
-            for i in range(img.shape[0]):
-                outputname = f"{basename}_section{i}.jpg"
-                section = img[i]
-                section = reduce_img(section, shrink_factor)
-                make_jpg_with_scalebar(section, pixel_size, args.outputdir, outputname)
+    with Pool(processes=args.numprocs) as pool:
+        pool.map(process_mrc, args.mrcfiles)
